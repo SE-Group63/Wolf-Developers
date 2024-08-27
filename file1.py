c8846dc541761d30e966144c557eca65fd408803
Rahil312 <76848658+Rahil312@users.noreply.github.com>
2024-08-27 23:26:50 +0000

    myfile push

--- file1.py
+++ file1.py
@@ -1,3 +1,21 @@
+def circle(radius):
+    pi = 3.14159
+    area = pi * radius ** 2
+    circumference = 2 * pi * radius
+    return area, circumference
 
-x = 1
-print("Homework ",x)
+def square(a):
+    area = a * a
+    perimeter = 4 * a
+    return area, perimeter
+
+# radius = 5
+# circle_area, circle_circumference = circle(radius)
+
+# length = 5
+# square_area, square_perimeter = square(length)
+
+# print(f"The area of the circle with radius {radius} is {circle_area}")
+# print(f"The circumference of the circle with radius {radius} is {circle_circumference}")
+# print(f"The area of the square with length {length} is {square_area}")
+# print(f"The perimeter of the square with length {length} is {square_perimeter}")
