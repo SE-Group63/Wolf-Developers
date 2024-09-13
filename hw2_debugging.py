c988934cbed9cce8892a41176ff995e52a879f8f
RohanChampaneri11 <75676632+RohanChampaneri11@users.noreply.github.com>
2024-09-13 00:23:09 +0000

    pylint  hw2_debugging

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,38 +1,43 @@
+"""hello welcome to ncsu"""
+
 import rand
 
 
-def mergeSort(arr : list[int]) -> list[int]:
-    if (len(arr) == 1):
+def merge_sort(arr: list[int]) -> list[int]:
+    """Merge sort"""
+    if len(arr) == 1:
         return arr
 
     half = len(arr) // 2
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    return recombine(merge_sort(arr[:half]), merge_sort(arr[half:]))
 
 
-def recombine(leftArr: list[int], rightArr: list[int]) -> list[int]:
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [0] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
+def recombine(left_arr: list[int], right_arr: list[int]) -> list[int]:
+    """Recombine"""
+    left_index = 0
+    right_index = 0
+    merged_arr = [0] * (len(left_arr) + len(right_arr))
+
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            right_index += 1
+            merged_arr[left_index + right_index] = left_arr[left_index]
         else:
-            leftIndex += 1
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+            left_index += 1
+            merged_arr[left_index + right_index] = right_arr[right_index]
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
+    for i in range(right_index, len(right_arr)):
+        merged_arr[left_index + right_index] = right_arr[i]
 
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    for i in range(left_index, len(left_arr)):
+        merged_arr[left_index + right_index] = left_arr[i]
 
-    return mergeArr
+    return merged_arr
 
 
-arr = rand.random_array([0] * 20)
-arr_out = mergeSort(arr)
+xtemp = rand.random_array([0] * 20)
+xfinal = merge_sort(xtemp)
 
-print(arr_out)
-#pyright: strict
+print(xfinal)
+# pyright: strict
