4600bba39112451a27b1958a146657b5932ea0df
Karanpatel-1603 <kpkp16012003@gmail.com>
2024-09-13 00:26:03 +0000

    HW1-testpass

--- tests/test_file1.py
+++ tests/test_file1.py
@@ -3,14 +3,12 @@ from file1 import square,circle
 
 def test_valid(): 
     assert square(5) == (25,20)
-    
-
-# def test_fail():
-#     assert square(5) == (78.5397,25)
 
 def test_fail(): 
-    assert circle(4) == (16,16)
+    assert circle(4) == (50.26544, 25.13272)
     
+# def test_fail():
+#     assert square(5) == (78.5397,25)
 
 # def test_fail():
 #     assert square(5) == (78.5397,31.4159)
