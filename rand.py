7df2d08508c82cd1252aab9665164d887d048d89
RohanChampaneri11 <75676632+RohanChampaneri11@users.noreply.github.com>
2024-09-13 00:15:58 +0000

    pylint rand

--- rand.py
+++ rand.py
@@ -1,10 +1,13 @@
+"""hello world"""
 import subprocess
 
 def random_array(arr: list[int]):
+    """temperarory"""
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i,_ in enumerate(arr):
+        shuffled_num = subprocess.run(
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check= True)
         arr[i] = int(shuffled_num.stdout.decode("utf-8").strip())
     return arr
 
-#pyright:strict
+# pyright:strict
